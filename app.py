1837317fb067f924a1eeb951d0ce80b47a5f5d91
naveen-badathala <66679493+naveen-badathala@users.noreply.github.com>
2022-05-05 01:15:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,11 +68,11 @@ if submitButton:
    #     examples.append(text_input3)
     #init_lr = 3e-5
     optimizer = optimization.create_optimizer(init_lr=3e-5,
-                                          num_train_steps=105,
-                                          num_warmup_steps=10,
+                                          #num_train_steps=105,
+                                          #num_warmup_steps=10,
                                           optimizer_type='adamw')
     #reloaded_model = tf.keras.models.load_model(models_dir + 'hypo_red_trained_bert_cased_e3.h5',  custom_objects = {'KerasLayer': hub.KerasLayer, 'AdamWeightDecay': optimizer})
-    reloaded_model = tf.keras.models.load_model(models_dir + 'hypo_red_trained_bert_cased_e3.h5', custom_objects = {'KerasLayer': hub.KerasLayer, 'AdamWeightDecay': 'adamw'})
+    reloaded_model = tf.keras.models.load_model(models_dir + 'hypo_red_trained_bert_cased_e3.h5', custom_objects = {'KerasLayer': hub.KerasLayer, 'AdamWeightDecay': optimizer})
     results = tf.sigmoid(reloaded_model(tf.constant(examples)))
     answer = "HI"
     st.markdown(
